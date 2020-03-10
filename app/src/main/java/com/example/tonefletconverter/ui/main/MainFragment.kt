c879d0f506cca5679ce02bd6e8fc19acb502a7e7
appseed246 <guitar.telecaster2017@gmail.com>
2020-03-10 23:42:34 +0900

    音階ボタン追加

--- app/src/main/java/com/example/tonefletconverter/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/tonefletconverter/ui/main/MainFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import com.example.tonefletconverter.R
 import com.example.tonefletconverter.databinding.MainFragmentBinding
-import com.example.tonefletconverter.model.TestState
 
 class MainFragment : Fragment() {
 
@@ -20,8 +17,6 @@ class MainFragment : Fragment() {
     }
 
     private val viewModel: MainViewModel by viewModels()
-
-    private lateinit var button: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,18 +31,6 @@ class MainFragment : Fragment() {
 
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
-
-        // 出題状態に応じてボタンに表示する文字を切り替える
-        button = binding.root.findViewById(R.id.button)
-        viewModel.state.observe(viewLifecycleOwner, Observer {
-            val stringRes = when (it) {
-                TestState.Question -> R.string.button_name_on_question
-                TestState.Answer -> R.string.button_name_on_answer
-                else -> throw IllegalStateException("invalid status: $it")
-            }
-            button.text = getString(stringRes)
-        })
-
         return binding.root
     }
 }
